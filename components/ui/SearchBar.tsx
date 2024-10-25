01fe1c993f5facbe08706c8b25bd2a78e2f59249
Md. Yamin <mohammadyamin80@gmail.com>
2024-10-25 21:58:44 +0600

    zod and react-hook-form added on search bar input field

--- components/ui/SearchBar.tsx
+++ components/ui/SearchBar.tsx
@@ -1,30 +1,48 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const querySchema = z.object({
+  searchQuery: z.string().min(1, { message: "Search query cannot be empty." }),
+});
+
+type QueryFormData = z.infer<typeof querySchema>;
 
 function SearchBar() {
-    const [query, setQuery] = useState("");
-    const router = useRouter();
+  const router = useRouter();
 
-  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(query)
-    router.push(`/search/${query}`);
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    formState: { errors },
+  } = useForm<QueryFormData>({
+    resolver: zodResolver(querySchema),
+  });
+
+  const onSubmit = (data: QueryFormData) => {
+    router.push(`/search/${data.searchQuery}`);
   };
 
   return (
-    <form onSubmit={handleSearch} className="flex items-center w-2/3 justify-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex items-center w-2/3 justify-center"
+    >
       <input
         type="text"
-        name="search"
         placeholder="Search Movies..."
-        className="p-2 rounded-l-md  text-black dark:bg-gray-700 dark:text-white w-3/5 focus:outline-none"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        {...register("searchQuery")}
+        className={`p-2 rounded-l-md w-3/5 text-black dark:bg-gray-700 dark:text-white focus:outline-none ${
+          errors.searchQuery ? "border border-red-500" : "border-transparent"
+        }`}
+        onBlur={() => clearErrors("searchQuery")}
       />
       <button
         type="submit"
-        className="bg-yellow-500 dark:bg-yellow-400  text-black py-2 px-4 rounded-r-md hover:bg-yellow-600 dark:hover:bg-yellow-500 transition"
+        className="bg-yellow-500 dark:bg-yellow-400 text-black py-2 px-4 rounded-r-md hover:bg-yellow-600 dark:hover:bg-yellow-500 transition"
       >
         Search
       </button>
